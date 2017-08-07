8f1ec277e303d1420f0310fb8964925f5e86db37
Pavel Zhuk <ppzhuk@gmail.com>
2017-08-07 18:23:17 +0300

    steering turns

--- src/main/kotlin/ru/ppzhuk/game/Mouse.kt
+++ src/main/kotlin/ru/ppzhuk/game/Mouse.kt
@@ -9,6 +9,8 @@ import javax.imageio.ImageIO
 
 class Mouse(x: Double, y: Double) : GameElement {
     private var position: Vector = Vector(x, y)
+    private var desiredVelocity: Vector = Vector(0.0, 0.0)
+    private var steeringForce: Vector = Vector(0.0, 0.0)
     private var velocity: Vector = Vector(0.0, 0.0)
     private val brain: FSM = FSM(this::findPoint3)
 
@@ -17,18 +19,27 @@ class Mouse(x: Double, y: Double) : GameElement {
     )
 
     companion object {
-        @JvmStatic
-        private val MAX_VELOCITY = 3.0
+        @JvmStatic private val MAX_VELOCITY = 3.0
+        @JvmStatic private val MAX_FORCE = 0.5
     }
 
-    constructor() : this(150.0, 100.0)
+    constructor() : this(150.0, 64.0)
 
     private fun findPoint1(): Unit = findPoint(Game.point1, this::findPoint2)
     private fun findPoint2(): Unit = findPoint(Game.point2, this::findPoint3)
     private fun findPoint3(): Unit = findPoint(Game.point3, this::findPoint1)
 
     private fun findPoint(point: Vector, secondState: () -> Unit) {
-        velocity = Vector.normalize(getRelativeVector(point.x, point.y), MAX_VELOCITY)
+        desiredVelocity = Vector.normalize(getRelativeVector(point.x, point.y), MAX_VELOCITY)
+        steeringForce = Vector.normalize(desiredVelocity - velocity, MAX_FORCE)
+
+        val diff = desiredVelocity - velocity
+        velocity = if (Math.abs(diff.x) < 0.5 && Math.abs(diff.y) < 0.5) {
+            desiredVelocity
+        } else {
+            Vector.normalize(velocity + steeringForce, MAX_VELOCITY)
+        }
+
         if (position.distance(point) < 10.0) {
             brain.state = secondState
         }
@@ -41,24 +52,18 @@ class Mouse(x: Double, y: Double) : GameElement {
     override fun redraw(g: Graphics) {
         val imgCenterX = img.getWidth(null) / 2.0
         val imgCenterY = img.getHeight(null) / 2.0
-        val rotateOperation = affineTransformOp(imgCenterX, imgCenterY)
+        val angle = getAngle()
+        val rotateOperation = AffineTransformOp(
+                AffineTransform.getRotateInstance(angle, imgCenterX, imgCenterY),
+                AffineTransformOp.TYPE_BILINEAR
+        )
         val x = this.position.x - imgCenterX
         val y = this.position.y - imgCenterY
         g.drawImage(rotateOperation.filter(img, null), x.toInt(), y.toInt(), null)
     }
 
-    private fun affineTransformOp(imgCenterX: Double, imgCenterY: Double): AffineTransformOp {
-        val tx = AffineTransform.getRotateInstance(getAngle(), imgCenterX, imgCenterY)
-        return AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR)
-    }
-
     private fun getAngle(): Double {
-        val vec = when (brain.state) {
-            this::findPoint1 -> getRelativeVector(Game.point1.x, Game.point1.y)
-            this::findPoint2 -> getRelativeVector(Game.point2.x, Game.point2.y)
-            this::findPoint3 -> getRelativeVector(Game.point3.x, Game.point3.y)
-            else -> throw IllegalStateException("Unknown Mouse state")
-        }
+        val vec = velocity
         val imgVec = Vector(0.0, 10.0)
 
         return Math.atan2(imgVec.x * vec.y - imgVec.y * vec.x, imgVec.x * vec.x + imgVec.y * vec.y)
